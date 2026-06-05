39c32b809bf00cf1371e72194d9ab8a14a9efb57
noname <you@example.com>
2026-06-05 15:48:52 -0400

    update 2026-06-05 15:48:52

---
+++ 8client_minimal_tcp_ip.py
@@ -0,0 +1,67 @@
+
+"""
+# minimal client py
+#!/usr/bin/env python3
+import socket
+
+s = socket.socket()
+s.connect(('127.0.0.1', 5025))
+s.send(b"*IDN?\n")
+print(s.recv(1024))
+s.close()
+"""
+
+
+
+
+# minimal VISA
+
+#!/usr/bin/env python3
+import pyvisa
+import time
+
+timestr = time.strftime("%y%m%d_%H%M%S")
+
+
+rm = pyvisa.ResourceManager()
+inst = rm.open_resource('TCPIP::raspi.local::5025::SOCKET')
+inst.write_termination = '\n'
+inst.read_termination = '\n'
+inst.timeout = 5000  # 1 seconds
+inst.chunk_size = 8*1024*1024  # read 4MB chunks (if supported)
+try:
+	while True:
+		
+		time.sleep(0.25)
+		
+		
+		response_simple_query = inst.query('IDN?')
+		print (response_simple_query)
+		
+		
+		response_header_of_dlog = inst.query('GET_LOG?')
+		print(f"recieved: {response_header_of_dlog}")
+		# FIX: Use read_raw() for binary data (not read())
+		datalog = inst.read_raw()
+		print(f"recieved log size {len(datalog)}\n")
+		# Append log dump from pi to file
+		with open('dump_log.log', 'ab') as f:  # 'ab' = append BINARY
+   		# Convert header string to bytes before writing 
+			f.write(response_header_of_dlog.encode() + datalog)
+		
+		
+	
+		
+		
+except pyvisa.errors.VisaIOError as e:
+    print(f"Timeout or error, not defined cmd: {e}")
+
+
+inst.close()
+
+
+
+
+
+
+
